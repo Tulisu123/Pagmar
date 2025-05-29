d1c2494b598de768802afc1e91aa52a1d5494980
Tal Peretz <tal.peretz@tufin.com>
2025-05-29 10:08:30 +0300

    video service edit back to normal api

--- src/videosService.js
+++ src/videosService.js
@@ -6,8 +6,11 @@ export const videosService = {
 }
 
 async function getVideos() {
-    const res = await fetch('/.netlify/functions/get-videos');
-    console.log(res)
-    const data = await res.json();
-    return data;
+  const res = await fetch(`https://api.pexels.com/videos/search?query=nature&per_page=${NUMBER_OF_VIDEOS}&size=medium&orientation=landscape`, {
+    headers: {
+      Authorization: API_KEY
+    }
+  })
+  const data = await res.json();
+  return data.videos
 }
