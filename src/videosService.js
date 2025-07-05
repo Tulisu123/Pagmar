46a59a2aa37a19f7ae85c534a2f3273b495f1215
Tal Peretz <tal.peretz@tufin.com>
2025-07-05 22:58:54 +0300

    restoring

--- src/videosService.js
+++ src/videosService.js
@@ -2,20 +2,21 @@ export const videosService = {
   getVideos,
 }
 
+function isMobileDevice() {
+  return /Mobi|Android|iPhone|iPad/i.test(navigator.userAgent)
+}
+
 async function getVideos() {
   const NUMBER_OF_VIDEOS = 12
-  const LOCAL_IDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] // אפשר לעדכן רק מה שכווץ שוב
+  const BASE_URL = 'https://pagmarpullzone.b-cdn.net'
+  const isMobile = isMobileDevice()
+
   const videoList = []
-
   for (let i = 1; i <= NUMBER_OF_VIDEOS; i++) {
-    const isLocal = LOCAL_IDS.includes(i)
-    const url = isLocal
-      ? `/videos/compressed_again_${i}.mp4`
-      : `https://pagmarpullzone.b-cdn.net/compressed_${i}.mp4`
-
+    const prefix = isMobile ? 'compressed_again_' : 'compressed_'
     videoList.push({
       id: i,
-      url,
+      url: `${BASE_URL}/${prefix}${i}.mp4`,
     })
   }
 
