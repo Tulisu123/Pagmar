5b690a000a80100df97955afd341f965b4fad449
Tal Peretz <tal.peretz@tufin.com>
2025-07-08 10:38:21 +0300

    new videos layout

--- src/videosService.js
+++ src/videosService.js
@@ -8,12 +8,12 @@ function isMobileDevice() {
 
 async function getVideos() {
   const NUMBER_OF_VIDEOS = 12
-  const BASE_URL = 'https://pagmarpullzone.b-cdn.net'
+  const BASE_URL = 'https://pagmarpullzone.b-cdn.net/final-layout' // עדכון לתיקייה החדשה
   const isMobile = isMobileDevice()
 
   const videoList = []
   for (let i = 1; i <= NUMBER_OF_VIDEOS; i++) {
-    const prefix = isMobile ? 'compressed_again_' : 'compressed_'
+    const prefix = isMobile ? 'compressed_final_mobile_' : 'compressed_final_desktop_'
     videoList.push({
       id: i,
       url: `${BASE_URL}/${prefix}${i}.mp4`,
